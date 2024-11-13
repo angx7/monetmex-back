457f47d000858c1fc95cbc54c9d2ca568abc18dc
Ángel Becerra <angelmirocu@gmail.com>
2024-11-13 15:40:13 -0600

    update

--- index.js
+++ index.js
@@ -13,77 +13,46 @@ app.use(cors());
 app.use(express.json());
 
 async function main() {
-  try {
-    const db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'monettest',
-    });
+  const maxRetries = 5;
+  const retryDelay = 5000;
+  let retries = 0;
 
-    // const createTableQuery = `
-    //   CREATE TABLE IF NOT EXISTS clientes (
-    //   clienteId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    //   nombreCliente VARCHAR(255) NOT NULL,
-    //   emailCliente VARCHAR(255) NOT NULL UNIQUE,
-    //   telefonoCliente VARCHAR(255) NOT NULL,
-    //   passwordCliente VARCHAR(255) NOT NULL,
-    //   bloqueoPagoCaja BOOLEAN NOT NULL
-    //   )
-    // `;
+  while (retries < maxRetries) {
+    try {
+      const db = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'monettest',
+      });
 
-    // await db.query(createTableQuery);
-    // console.log('Table created');
+      routerApp(app, db, bcrypt, saltRounds);
+      app.use(logErrors);
+      app.use(errorHandler);
 
-    routerApp(app, db, bcrypt, saltRounds);
-    app.use(logErrors);
-    app.use(errorHandler);
+      app.get('/', (req, res) => {
+        res.send('Welcome to the API');
+      });
 
-    app.get('/', (req, res) => {
-      res.send('Welcome to the API');
-    });
+      console.log('Connected to database');
 
-    console.log('Connected to database');
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
 
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    console.error('Error connecting to the database: ', error);
+      break;
+    } catch (error) {
+      retries++;
+      console.error('Error connecting to the database: ', error);
+
+      if (retries === maxRetries) {
+        console.error('Max retries reached. Exiting...');
+        process.exit(1);
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+    }
   }
 }
 
 main();
-
-// db.connect((err) => {
-//   if (err) {
-//     throw new Error('No se pudo conectar: ' + err);
-//   }
-//   console.log('Connected to database');
-// });
-
-// const createTablrQuery = `CREATE TABLE IF NOT EXISTS clientes (
-// clienteId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-// nombreCliente VARCHAR(255) NOT NULL,
-// emailCliente VARCHAR(255) NOT NULL,
-// telefonoCliente VARCHAR(255) NOT NULL,
-// passwordCliente VARCHAR(255) NOT NULL,
-// bloqueoPagoCaja BOOLEAN NOT NULL
-// )`;
-
-// db.query(createTablrQuery, (err, result) => {
-//   if (err) {
-//     throw new Error('No se pudo crear la tabla: ' + err);
-//   }
-//   console.log('Table created');
-// });
-
-// routerApp(app); // si truena es porque le faltan parametros como db, bcrypt, saltRounds
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the API');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
